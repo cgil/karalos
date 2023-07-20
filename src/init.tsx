c6dc2e03cd53bd091f2a929ead79338da66f89ea
Carlos Gil <cg@carlosmgil.com>
2023-07-20 13:49:01 -0400

    chore: Fix BrowserRouter

--- src/init.tsx
+++ src/init.tsx
@@ -6,10 +6,9 @@ const NotFoundPage = React.lazy(() => import("./routes/errors/not-found-page"));
 
 export const Init = (): JSX.Element | null => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route element={<HomePage />} path="/" />
-        <Route element={<Navigate to="/" />} path="/karalos" />
         <Route element={<NotFoundPage />} path="/404" />
         <Route element={<Navigate to="/404" />} path="*" />
       </Routes>
